84d791ae79914f8a8733d50d7005484a45892ef9
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2023-06-06 15:51:50 -0300

    Covariancia

---
+++ CSharpCollectionsParte2/09-Covariancia/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.WriteLine("string para object");
+string titulo = "meses";
+object obj = titulo;
+Console.WriteLine(obj);
+
+Console.WriteLine("List<string> para List<object>");
+
+IList<string> listaMeses = new List<string>
+{
+    "Janeiro", "Fevereiro", "Março",
+    "Abril", "Maio", "Junho",
+    "Julho", "Agosto", "Setembro",
+    "Outubro", "Novembro", "Dezembro"
+};
+//IList<object> listaObj = listaMeses;
+Console.WriteLine();
+
+Console.WriteLine("string[] para object[]?");
+string[] arrayMeses = new string[]
+{
+    "Janeiro", "Fevereiro", "Março",
+    "Abril", "Maio", "Junho",
+    "Julho", "Agosto", "Setembro",
+    "Outubro", "Novembro", "Dezembro"
+};
+object[] arrayObj = arrayMeses;
+
+//Console.WriteLine(arrayObj);
+
+foreach (var item in arrayObj)
+{
+    Console.WriteLine(item);
+}
+
+arrayObj[0] = "PRIMEIRO MÊS";
+Console.WriteLine(arrayObj[0]);
+Console.WriteLine();
+
+//arrayObj[0] = 12345;
+//Console.WriteLine(arrayObj[0]);
+//Console.WriteLine();
+
+
+IEnumerable<object> enumObj = listaMeses; //COVARIÂNCIA
+foreach (var item in enumObj)
+{
+    Console.WriteLine(item);
+}
